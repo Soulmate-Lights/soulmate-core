3eaa711772cbba245b333917f1690511ab3f22ee
Elliott Kember <elliott.kember@gmail.com>
2019-12-06 01:01:35 -0800

    5.23

--- SoulmateMain.h
+++ SoulmateMain.h
@@ -13,7 +13,7 @@
 #include "./circadian.h"
 #include "./settings.h"
 
-#define SOULMATE_VERSION "5.22"
+#define SOULMATE_VERSION "5.23"
 #define MAX_NUMBER_OF_ROUTINES 25
 void FastLEDshowTask(void* pvParameters);
 
