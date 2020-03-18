93fbbe461c654938fda1b78806848ec3101f23a7
Elliott Kember <elliott.kember@gmail.com>
2020-03-18 13:15:27 -0700

    Remove some commentS

--- soulmateTime.h
+++ soulmateTime.h
@@ -41,31 +41,4 @@ int minuteNow(double secondsAtStart) {
   return minute(seconds, secondsAtStart);
 }
 
-// String nameOfDay(int day) {
-//   switch (day) {
-//     case 0:
-//       return "Sunday";
-//       break;
-//     case 1:
-//       return "Monday";
-//       break;
-//     case 2:
-//       return "Monday";
-//       break;
-//     case 3:
-//       return "Tuesday";
-//       break;
-//     case 4:
-//       return "Thursday";
-//       break;
-//     case 5:
-//       return "Friday";
-//       break;
-//     case 6:
-//       return "Saturday";
-//       break;
-//   }
-//   return "None";
-// }
-
 #endif
