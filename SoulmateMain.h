b7036121e4d01ddae1d57f2b3c623a4fc148da0e
Elliott Kember <elliott.kember@gmail.com>
2020-09-12 13:35:44 -0700

    Try writing to preferences

--- SoulmateMain.h
+++ SoulmateMain.h
@@ -547,7 +547,7 @@ public:
 
       Preferences preferences;
       preferences.begin("testing", false);
-      preferences.Int("brightness", brightness)
+      preferences.putInt("brightness", brightness)
       preferences.end();
     }
 
