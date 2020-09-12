bac0dbaf623c5809d79c6bf036ab5bed94f428c7
Elliott Kember <elliott.kember@gmail.com>
2020-09-12 13:56:21 -0700

    spiffs

--- SoulmateMain.h
+++ SoulmateMain.h
@@ -574,7 +574,6 @@ public:
 
     if (root.containsKey("brightness")) {
       int brightness = static_cast<int>(root["brightness"]);
-      SoulmateSettings::saveBrightness(brightness);
       setBrightness(brightness);
     }
 
