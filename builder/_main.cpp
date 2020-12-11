ee6f17e30c71b6957ae0543d81fe3c59f461e5cf
Elliott Kember <elliott.kember@gmail.com>
2020-12-11 09:38:00 -0800

    Updated main.cpp

--- builder/_main.cpp
+++ builder/_main.cpp
@@ -1,20 +1,24 @@
-#define FIRMWARE_NAME "livingroom"
-#define N_LEDS 1200
+#define FIRMWARE_NAME "soulmate"
+#define N_LEDS 64
 #define SOULMATE_MILLIAMPS 3000
 #define LED_TYPE WS2812B
 #define FADE_DURATION 10
-#define LED_COLS 1200
-#define LED_ROWS 1
+#define LED_COLS 8
+#define LED_ROWS 8
+#define SOULMATE_DATA_PIN 27
+#define SOULMATE_CLOCK_PIN 29
 #define USE_WS2812B true
-#define USE_BLUETOOTH true
 
 #include <Soulmate.h>
 
 void pattern() {
+  for (int i = 0; i < N_LEDS; i++) {
+    Soulmate.leds[i] = CRGB::Red;
+  }
 }
 
 void setup() {
-  Soulmate.addRoutine("pattern", pattern);
+  Soulmate.addRoutine("Pattern", pattern);
 
   Soulmate.setup();
 }
