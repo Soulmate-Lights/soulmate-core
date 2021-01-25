bbb15be3cbbe94e0e844048d487a7bcafd844980
Elliott Kember <elliott.kember@gmail.com>
2021-01-24 18:50:03 -0800

    tidyup

--- SoulmateFiles.h
+++ SoulmateFiles.h
@@ -9,18 +9,10 @@
 #include <Preferences.h>
 Preferences filesPreferences;
 
-// TODO(elliott): Change these to use "preferences" instead of SPIFFS
-
 void writeFile(String name, String contents) {
   filesPreferences.begin("settings", false);
   filesPreferences.putString(name.c_str(), String(contents));
   filesPreferences.end();
-// #if defined(ESP32) || defined(ESP8266)
-//   File f = SPIFFS.open(name, "w");
-//   f.print(contents);
-//   f.flush();
-//   f.close();
-// #endif
 }
 
 String readFile(String name) {
@@ -28,32 +20,12 @@ String readFile(String name) {
   String value = filesPreferences.getString(name.c_str(), "");
   filesPreferences.end();
   return value;
-
-//   String ret;
-// #if defined(ESP32) || defined(ESP8266)
-  // File f = SPIFFS.open(name, "r");
-  // if (!f)
-  //   return "";
-  // ret = f.readString();
-
-  // String newString;
-  // for (int i = 0; i < ret.length(); i++) {
-  //   char inchar = ret[i];
-  //   if ((' ' <= inchar) && (inchar <= '~')) newString += inchar;
-  // }
-
-  // ret = newString;
-
-  // f.close();
-// #endif
-  // return ret;
 }
 
 void deleteAllFiles() {
   filesPreferences.begin("settings", false);
   filesPreferences.clear();
   filesPreferences.end();
-  // SPIFFS.format();
 }
 
 #endif // BUILDER_LIBRARIES_SOULMATE_FILES_H_
