7d5b6aca8ffd6724327ed2bb72c3eafa6a810c2e
Elliott Kember <elliott.kember@gmail.com>
2021-02-10 16:48:08 -0800

    Simplify

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { exec, execSync } = require("child_process");
+const bodyParser = require("body-parser");
+const path = require("path");
+const os = require("os");
+const fs = require("fs").promises;
+const { promisify } = require("util");
+const rimraf = require("rimraf");
+const cors = require("cors");
+const { v4 } = require("uuid");
+const rm = promisify(rimraf);
+
+const jobs = Math.max(1, os.cpus().length - 1);
+const app = express();
+app.use(bodyParser.json());
+app.options("/build", cors());
+const port = process.env.PORT || 8081;
+const encoding = "utf-8";
+
+function execAsync(cmd) {
+  return new Promise((resolve) => {
+    exec(cmd, (error, stdout, stderr) => resolve({ error, stdout, stderr }));
+  });
+}
+
+// try {
+//   child_process.execSync("git pull");
+// } catch {
+//   console.log("Pulling failed");
+// }
+
+const compile = async (code) => {
+  const id = v4();
+  const sketchDir = path.join(__dirname, "output", id);
+  const mainDir = path.join(sketchDir, "main");
+
+  execSync(`cp -r ./builder ${sketchDir}`);
+  await fs.writeFile(path.join(mainDir, "main.cpp"), code, { encoding });
+
+  const command = `make -j${jobs} -C ${sketchDir}`;
+  console.time(command);
+  const { error, stderr, stdout } = await execAsync(command);
+  console.timeEnd(command);
+
+  if (error) {
+    console.log(error);
+    execSync(`rm -rf ${sketchDir}`);
+    return { error, stderr };
+  }
+
+  const binFile = path.join(sketchDir, "build", `soulmate.bin`);
+  const builtBinFile = `/${id}.bin`;
+  execSync(`cp ${binFile} ${builtBinFile}`);
+  execSync(`rm -rf ${sketchDir}`);
+  return { stdout, binFile: builtBinFile };
+};
+
+app.post("/build", cors(), async (req, res) => {
+  const { error, binFile } = await compile(req.body.sketch);
+
+  if (error) {
+    res.status(500).send(stderr);
+  } else {
+    res.sendFile(binFile, {}, () => {
+      execSync(`rm -rf ${binFile}`);
+    });
+  }
+});
+
+app.listen(port, () => console.log("App ready, listening on port", port));
---
+++ run.sh
@@ -0,0 +1,2 @@
+docker build --tag soulmate-dev .
+docker run -p 8081:8081 --rm soulmate-dev
