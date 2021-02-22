9f3125de2925e887054f3f58120bd2af9803da36
Elliott Kember <elliott.kember@gmail.com>
2021-02-22 08:53:18 -0800

    Disable a bunch of warnings wholesale

---
+++ main/component.mk
@@ -0,0 +1,1 @@
+CXXFLAGS += -Wall -Werror -Wno-error=unused-function -Wno-error=unused-but-set-variable -Wno-error=unused-variable -Wno-error=deprecated-declarations -Wno-error=parentheses -Wno-error=sizeof-pointer-memaccess -Wno-error=unused-value -Wno-error=type-limits
