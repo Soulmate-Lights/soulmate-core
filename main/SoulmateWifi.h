1c8f5db877b04733bf385fc6f206326f6bacb620
Elliott Kember <elliott.kember@gmail.com>
2021-02-22 12:18:36 -0800

    disable sleep

--- main/SoulmateWifi.h
+++ main/SoulmateWifi.h
@@ -67,7 +67,8 @@ void delayAndConnectWayLater(void *parameter) {
   if (!ssid.equals("")) {
     Serial.println("[Soulmate-Wifi] Set STA mode...");
     WiFi.mode(WIFI_STA);
-    WiFi.setSleep(false);
+    // Disabled due to errors with esp-idf 3.3.4
+    // WiFi.setSleep(false);
     vTaskDelay(1000 / portTICK_PERIOD_MS);
 
     Serial.println("[Soulmate-Wifi] WiFi.begin()...");
